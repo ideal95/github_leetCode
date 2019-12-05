35f0ae629a974d5fbd26c7d333744fe7e7aabf51
lixiang <15996303910@163.com>
2019-12-05 09:23:25 +0800

    1、20有效的括号 2、第一个只出现一次的字母

---
+++ Leetcode/src/easy/FirstWord.java
@@ -0,0 +1,38 @@
+package easy;
+
+/**
+ * @Description:
+ * @Author: lixiang
+ * @Date: 2019-12-4 17:19
+ * String a="asdsadsadsadsadsadsad"
+ * a-z 获取第一个不重复的字母
+ *
+ */
+public class FirstWord {
+
+    public static char getFirstWord(String string){
+//        如果字符串为空 直接返回0
+        if(string==null||string.length()==0){
+            return '0';
+        }
+//        建立一个对应的a-z 的数组
+        int length= string.length();
+        int []nums=new int[26];
+        for(int i=0;i<length;i++){
+//            ‘a’=97; 默认每个初始值为1 一旦重复就大于1
+            nums[string.charAt(i)-'a']++;
+        }
+        for (int i = 0; i <length ; i++) {
+//            如果存在nums==1的情况下;那么该字母只出现一次
+            if(nums[string.charAt(i)-'a']==1){
+                return string.charAt(i);
+            }
+        }
+        return '0';
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(getFirstWord("abcde"));
+    }
+}
---
+++ Leetcode/src/easy/IsValid.java
@@ -0,0 +1,67 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * @Description:
+ * @Author: lixiang
+ * @Date: 2019-12-4 18:02
+ * leetcode:20 有效的括号
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * 有效字符串需满足：
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ */
+public class IsValid {
+ private HashMap<Character,Character> map;
+    public IsValid(){
+        this.map=new HashMap<>();
+        this.map.put(')','(');
+        this.map.put('}','{');
+        this.map.put(']','[');
+    }
+//    暴力执法 、贼慢
+
+    public boolean method1(String s) {
+        if(s.length()==0){
+            return true;
+        }
+        if(s.length()%2==1){
+            return false;
+        }
+        while(s.contains("()") || s.contains("[]") || s.contains("{}")){
+            s=s.replace("()","");
+            s=s.replace("{}","");
+            s=s.replace("[]","");
+
+        }
+        return s.length()==0;
+    }
+
+    public boolean method2(String s){
+        if(s==null||s.length()==0){
+            return true ;
+        }
+        if(s.length()%2==1){
+            return false ;
+        }
+        Stack<Character> stack=new Stack<>();
+        for (int i = 0; i <s.length() ; i++) {
+            if(map.containsKey(s.charAt(i))){
+                char ele=stack.empty()? '#':stack.pop();
+                if(ele!=this.map.get(s.charAt(i))){
+                        stack.push(s.charAt(i));
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Stack<Character> stack=new Stack<>();
+        System.out.println(stack.empty());
+    }
+}
