6a58c24f019805778b6193d4620ff93ac9722c61
lixiang <15996303910@163.com>
2019-12-16 11:24:20 +0800

    README新增leetcode指引

---
+++ Leetcode/src/easy/LengthOfLastWord.java
@@ -0,0 +1,35 @@
+package easy;
+
+/**
+ * @author : lixiang
+ * @date : 2019-12-16 10:23
+ * 给定一个仅包含大小写字母和空格 ' ' 的字符串，返回其最后一个单词的长度。
+ * <p>
+ * 如果不存在最后一个单词，请返回 0 。
+ * <p>
+ * 说明：一个单词是指由字母组成，但不包含任何空格的字符串。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: "Hello World"
+ * 输出: 5
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/length-of-last-word
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LengthOfLastWord {
+
+
+    public static int lengthOfLastWord(String s) {
+//        先去掉头尾的空格；
+        s = s.trim();
+        if (s.length() == 0) {
+            return 0;
+        }
+//        找最后一个空格的下标
+        int index = s.lastIndexOf(" ");
+        return s.length() - index - 1;
+    }
+
+}
--- README.md
+++ README.md
@@ -6,4 +6,5 @@ easy：
 20、有效的括号 IsValid
 26、删除数组中的重复项 RemoveDuplicates
 27、移除元素 RemoveElement
+58、最后一个单词的长度 LengthOfLastWord
 283、移动零 MoveZeros
