2d69a3c1662b01d801d23d9f8d0c1618156af2a1
lixiang <15996303910@163.com>
2019-12-18 18:33:12 +0800

    leetcode新增:   选择插入二分法 ；

--- Leetcode/src/easy/SearchInsert.java
+++ Leetcode/src/easy/SearchInsert.java
@@ -37,4 +37,19 @@ public class SearchInsert {
         }
         return index;
     }
+
+    //    二分法
+    public int searchInsert2(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
 }
